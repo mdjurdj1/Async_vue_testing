a61fb2d3a6495229fc0d1dfe46e076c05fd94962
Mdjurdj1 <mdjurdj1@gmail.com>
2017-10-28 15:01:05 -0400

    Added bound html fields

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var state = {}
+
 var app = new Vue({
   el: '#app',
   data: {
@@ -9,16 +11,18 @@ var app_2 = new Vue({
   el: '#app_2',
   data: {
     message: 'Wow, another message!',
+    input: 'This field is bound from the Vue.',
     github: function() {
       var myInit = {
         method: 'GET',
         headers: {
-          Authorization: `token INSERTTOKEHERE`
+          Authorization: `token `
         }
       }
       fetch('https://api.github.com/users/mdjurdj1/repos', myInit).then(response => {
         return response.json()
       }).then(json => {
+        state.name = json[0].owner.login
         json.forEach(r => {
           let node = document.createElement('LI')
           let textNode = document.createTextNode(`${r.name}`)
@@ -30,6 +34,18 @@ var app_2 = new Vue({
 }
 })
 
-handleSubmit = () => {
-  alert('hey')
+var app_3 = new Vue({
+  el: '#app_3',
+  data: {
+     message: `Greetings! You loaded this page on ${new Date().toLocaleString()}!`
+  }
+})
+
+handleSubmit = (e) => {
+  state.submitted ? state.submitted = false : state.submitted = true
+  if(state.submitted) {
+    app_2.input = 'THE SWITCH IS ON!'
+  } else {
+    app_2.input = 'The switch is off...'
+  }
 }
